3672eef28401eb92c32658c73669315dded7a117
devopsxxi <116847508+devopsxxi@users.noreply.github.com>
2022-10-29 15:22:44 -0500

    Provocando error

--- src/main/java/org/soyphea/k8s/controller/Controller.java
+++ src/main/java/org/soyphea/k8s/controller/Controller.java
@@ -12,7 +12,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.security.*;
+
+import javax.crypto.Cipher;
+import java.security.NoSuchAlgorithmException;
+import javax.crypto.NoSuchPaddingException;
 
 @Slf4j
 @RestController
@@ -39,9 +42,18 @@ public class Controller {
         return userService.getUser(containName);
     }
 
-   private void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        URL url = new URL(req.getParameter("url"));
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection(); // Noncompliant
+   private void test() {
+        try {
+            Cipher c1 = Cipher.getInstance("DES"); // Noncompliant: DES works with 56-bit keys allow attacks via exhaustive search
+            Cipher c7 = Cipher.getInstance("DESede"); // Noncompliant: Triple DES is vulnerable to meet-in-the-middle attack
+            Cipher c13 = Cipher.getInstance("RC2"); // Noncompliant: RC2 is vulnerable to a related-key attack
+            Cipher c19 = Cipher.getInstance("RC4"); // Noncompliant: vulnerable to several attacks (see https://en.wikipedia.org/wiki/RC4#Security)
+            Cipher c25 = Cipher.getInstance("Blowfish"); // Noncompliant: Blowfish use a 64-bit block size makes it vulnerable to birthday attacks
+
+            NullCipher nc = new NullCipher(); // Noncompliant: the NullCipher class provides an "identity cipher" one that does not transform or encrypt the plaintext in any way.
+        }
+        catch(NoSuchAlgorithmException|NoSuchPaddingException e) {
+        }
     }
 
 }
